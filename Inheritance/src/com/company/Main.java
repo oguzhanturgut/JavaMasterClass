966b726c8fb6c5a1b86692a2ff466fff7a815f39
Oguzhan <oguzhanturgut@outlook.com>
2020-03-28 21:30:54 +0100

    OOP

---
+++ Inheritance/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Animal", 1, 1, 5, 5);
+
+
+        Dog myDog = new Dog("hav",  55, 23.0,2,4,1,20,"asdasd");
+//        myDog.eat();
+//        myDog.walk();
+        myDog.run();
+
+    }
+
+}
---
+++ OOP Challenge/src/com/company/DeluxeBurger.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class DeluxeBurger extends Hamburger {
+    public DeluxeBurger() {
+        super("Deluxe", "Sausage & Bacon", 14.54, "White");
+        super.addHamburgerAddition1("Chips",2.75);
+        super.addHamburgerAddition2("Drink",1.81);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) { super.addHamburgerAddition1(name, price);
+        System.out.println("Can not add additional items to a deluxe burger");
+
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("Can not add additional items to a deluxe burger");
+
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) { super.addHamburgerAddition3(name, price);
+        System.out.println("Can not add additional items to a deluxe burger");
+
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("Can not add additional items to a deluxe burger");
+
+    }
+}
