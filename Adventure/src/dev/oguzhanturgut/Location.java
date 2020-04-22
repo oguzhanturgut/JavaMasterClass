584fd195513259ffc91dcf691c0aa82c4377e534
Oguzhan <oguzhanturgut@outlook.com>
2020-04-22 15:48:56 +0200

    Immutable Classes

--- Adventure/src/dev/oguzhanturgut/Location.java
+++ Adventure/src/dev/oguzhanturgut/Location.java
@@ -8,16 +8,20 @@ public class Location {
     private final String description;
     private final Map<String, Integer> exits;
 
-    public Location(int locationId, String description) {
+    public Location(int locationId, String description, Map<String,Integer> exits) {
         this.locationId = locationId;
         this.description = description;
-        this.exits = new HashMap<>();
+        if (exits != null) {
+            this.exits = new HashMap<>(exits);
+        } else {
+            this.exits = new HashMap<>();
+        }
         this.exits.put("Q", 0);
     }
 
-    public void addExit(String direction, int location) {
-        exits.put(direction, location);
-    }
+//    public void addExit(String direction, int location) {
+//        exits.put(direction, location);
+//    }
 
     public int getLocationId() {
         return locationId;
