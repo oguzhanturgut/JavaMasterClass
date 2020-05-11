5056d1192122981121f143555bee1a0ffbd135e2
Oguzhan <oguzhanturgut@outlook.com>
2020-05-11 02:08:30 +0200

    Set and Hash Set - equal and hashCode override

---
+++ SetAndHashSet/src/dev/oguzhanturgut/Main.java
@@ -0,0 +1,80 @@
+package dev.oguzhanturgut;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    private static Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static Set<HeavenlyBody> planets = new HashSet<>();
+
+    public static void main(String[] args) {
+        HeavenlyBody temp = new HeavenlyBody("Mercury", 88);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Venus", 225);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Earth", 365);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        temp = new HeavenlyBody("Saturn", 10759);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Uranus", 30660);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Neptune", 165);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Pluto", 248);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+
+        System.out.println("Planets");
+        for (HeavenlyBody planet : planets) {
+            System.out.println("\t" + planet.getName());
+
+        }
+
+        HeavenlyBody body = solarSystem.get("Earth");
+        System.out.println("Moons of " + body.getName());
+        for (HeavenlyBody satellite : body.getSatellites()) {
+            System.out.println("\t" + satellite.getName());
+        }
+
+        Set<HeavenlyBody> moons = new HashSet<>();
+        for (HeavenlyBody planet : planets) {
+            moons.addAll(planet.getSatellites());
+        }
+
+        System.out.println("All Moons");
+        for (HeavenlyBody moon : moons) {
+            System.out.println("\t" + moon.getName());
+        }
+
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+
+        for (HeavenlyBody planet : planets) {
+            System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+        }
+
+        Object obj = new Object();
+        System.out.println(obj.equals(obj));    //true
+        System.out.println("pluto".equals("")); //false
+
+    }
+}
